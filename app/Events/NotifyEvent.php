1c0073819e6fd19b191bc1bec068aef5678a08ec
Nguyễn Hồng Hải <nguyenhonghai1997bg@gmail.com>
2018-08-28 09:35:24 +0700

    fix realtime

--- app/Events/NotifyEvent.php
+++ app/Events/NotifyEvent.php
@@ -12,7 +12,9 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class NotifyEvent implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
